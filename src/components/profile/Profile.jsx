578207c3d62e645869305287f3ea063bdd2368b5
vicgriffin123 <129837311+vicgriffin123@users.noreply.github.com>
2024-06-15 19:19:19 +0300

    create icons

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,9 @@
-
 import React from 'react';
 import "./profile.css";
+import { CiLocationOn } from "react-icons/ci";
+import { RiGitRepositoryFill } from "react-icons/ri";
+import { IoIosPeople } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
 
 function Profile({ profile }) {
   if (!profile) {
@@ -8,15 +11,17 @@ function Profile({ profile }) {
   }
 
   return (
-    <div className="profile-info">
+    <div className="profile">
       <h2>{profile.name}</h2>
       <img src={profile.avatar_url} alt="Profile Avatar" width={100} />
+      <button><a href="">view on github</a></button>
       <p><strong>Bio:</strong> {profile.bio}</p>
-      <p><strong>Location:</strong> {profile.location}</p>
-      <p><strong>Public Repos:</strong> {profile.public_repos}</p>
-      <p><strong>Followers:</strong> {profile.followers}</p>
-      <p><strong>Following:</strong> {profile.following}</p>
-      <p><strong>Profile URL:</strong> <a href={profile.html_url} target="_blank" rel="noopener noreferrer">{profile.html_url}</a></p>
+      <p><strong><CiLocationOn />Location:</strong> {profile.location}</p>
+      <p><strong><RiGitRepositoryFill />Public Repos:</strong> {profile.public_repos}</p>
+      <p><strong><IoIosPeople />Followers:</strong> {profile.followers}</p>
+      <p><strong><IoIosPeople />Following:</strong> {profile.following}</p>
+      <p><strong><CiSearch />Profile URL:</strong> <a href={profile.html_url} target="_blank" rel="noopener noreferrer">{profile.html_url}</a></p>
+      
     </div>
   );
 }
